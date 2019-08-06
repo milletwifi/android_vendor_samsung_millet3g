e73feb9a41127f1cbb290394194e6b18e0fd64d0
Kevin Castellanos <castellanos.kevin@rocketmail.com>
2019-08-06 10:35:52 -0400

    Removing more entry

--- millet3g-vendor.mk
+++ millet3g-vendor.mk
@@ -23,7 +23,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/millet3g/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/samsung/millet3g/proprietary/lib/libmdmdetect.so:system/lib/libmdmdetect.so \
     vendor/samsung/millet3g/proprietary/lib/libreference-ril.so:system/lib/libreference-ril.so \
-    vendor/samsung/millet3g/proprietary/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
     vendor/samsung/millet3g/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
     vendor/samsung/millet3g/proprietary/lib/hw/vibrator.default.so:lib/hw/vibrator.default.so \
     vendor/samsung/millet3g/proprietary/vendor/lib/libconfigdb.so:system/vendor/lib/libconfigdb.so \
